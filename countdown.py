0eec13ab3738d485d994ce501fc99b3886c0c755
mein9 <mateo0221@gmail.com>
2024-05-26 11:25:05 -0700

    add protection against non integer inputs

--- countdown.py
+++ countdown.py
@@ -1,13 +1,21 @@
 import time
 
-def countdown(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r", flush=True)
-        time.sleep(1)
-        t -= 1
-    print('Blast off!!!')
-
-user_time = int(input("Enter the number of seconds: "))
-countdown(user_time)
+def countdown(time_input):
+    if isinstance(time_input, int):
+        while time_input:
+            mins, secs = divmod(time_input, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r", flush=True)
+            time.sleep(1)
+            time_input -= 1
+        print('Blast off!!!')
+    else:
+        print("Please enter a number")
+while True:
+    user_time = input("Enter the number of seconds: ")
+    if user_time.isdigit():
+        user_time = int(user_time)
+        countdown(user_time)
+        break
+    else:
+        print("Please enter a valid number.")
