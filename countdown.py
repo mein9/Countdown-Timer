24055575a98760eef0246ad92f8de8fceabc6d98
mein9 <mateo0221@gmail.com>
2024-05-26 11:37:40 -0700

    removed some redundant code

--- countdown.py
+++ countdown.py
@@ -1,16 +1,14 @@
 import time
 
 def countdown(time_input):
-    if isinstance(time_input, int):
-        while time_input:
-            mins, secs = divmod(time_input, 60)
-            timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(timer, end="\r", flush=True)
-            time.sleep(1)
-            time_input -= 1
-        print('Blast off!!!')
-    else:
-        print("Please enter a number")
+    while time_input:
+        mins, secs = divmod(time_input, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r", flush=True)
+        time.sleep(1)
+        time_input -= 1
+    print('Blast off!!!')
+    
 while True:
     user_time = input("Enter the number of seconds: ")
     if user_time.isdigit():
